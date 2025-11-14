4979413930f4930467ffccd3f8b32a4292a452dd
a.girsa <a-lex@girsa.ru>
2025-11-14 03:17:04 +0100

    Remove output file tracing and related webpack configurations from Next.js setup to simplify the configuration and prevent recursion issues on Vercel.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Отключаем outputFileTracing для избежания проблем с рекурсией на Vercel
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
-  // Используем более простой подход к trace
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Исключаем проблемные директории из серверного бандла
-      config.watchOptions = {
-        ...config.watchOptions,
-        ignored: ['**/.next/**', '**/.git/**', '**/.vercel/**', '**/node_modules/**'],
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
